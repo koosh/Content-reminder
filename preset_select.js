7ffadc57b69022c6acd19fc6f4db2482c5375030
Reigo <reigok@gmail.com>
2011-12-20 11:56:36 +0200

    Content reminder module for Drupal 7

---
+++ preset_select.js
@@ -0,0 +1,19 @@
+(function ($) {
+  Drupal.behaviors.content_reminder_preset_select = {
+    attach: function (context, settings) {
+      $('.content-reminder-precise-date', context).focus(); // the datepicker is attached on the first focus
+      
+      $('.content-reminder-preset-select', context).change(function () {
+        var timestamp = parseInt($(this).val());
+        if(timestamp) {
+          var date = new Date(timestamp * 1000);
+          $('.content-reminder-precise-date', context).datepicker('setDate', date);
+        }
+      });
+      
+      $('.content-reminder-precise-date', context).change(function () {
+        $('.content-reminder-preset-select', context).val(0);
+      });
+    }
+  };
+}(jQuery));
